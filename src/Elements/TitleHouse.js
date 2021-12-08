ad82646981a261de40e734f11c8a0774f51f283a
g0321143 <g0321143@iwate-u.ac.jp>
2021-12-08 10:27:04 +0900

    Titleのモデルの変更

--- src/Elements/TitleHouse.js
+++ src/Elements/TitleHouse.js
@@ -4,33 +4,110 @@ import { Environment, Stage, PerspectiveCamera, useGLTF, OrbitControls } from "@
 
 import colors from '../Constants/Color';
 
+useGLTF.preload('./Models/House.glb');
+
 function House(props) {
-    const { scene } = useGLTF("./Models/House.glb");
-    // scene.traverse((object) => {
-    //     if (object.isMesh) {
-    //         console.log(object.name);
-    //     }
-    // });
+    const { scene, nodes } = useGLTF('./Models/House.glb');
+
+    let house = [];
+
+    scene.traverse((object) => {
+        if (object.isMesh) {
+            if (object.name == "Cylinder011" || object.name == "Cube039_2" || object.name == "Cube041_2" ||
+                object.name == "Cylinder012" || object.name == "Cube036_1" || object.name == "Cube026_1") {
+                house.push(
+                    <mesh
+                        receiveShadow={true}
+                        rotation={object.rotation}
+                        scale={object.scale}
+                        position={object.position}
+                        geometry={object.geometry}
+                    >
+                        <meshStandardMaterial color={'white'} />
+                    </mesh>
+                );
+            } else {
+                house.push(
+                    <mesh
+                        receiveShadow={true}
+                        rotation={object.rotation}
+                        scale={object.scale}
+                        position={object.position}
+                        geometry={object.geometry}
+                    >
+                        <meshStandardMaterial color={'#F3D393'} />
+                    </mesh>
+                );
+            }
+        }
+    });
+
 
     return (
-        <>
-            <primitive object={scene} />
-        </>
+        <primitive object={scene} scale={1} position={props.position} />
+    )
+}
+
+
+function Rock(props) {
+    const { scene, nodes } = useGLTF("./Models/Rock.glb");
+
+    return (
+        <group {...props} dispose={null}>
+            <mesh
+                receiveShadow={true}
+                scale={nodes.Cube014.scale}
+                geometry={nodes.Cube014.geometry}
+            >
+                <meshStandardMaterial color={'#F3D393'} />
+            </mesh>
+        </group>
+    )
+}
+
+function Fence1(props) {
+    const { scene, nodes } = useGLTF("./Models/Fence1.glb");
+    scene.traverse((object) => {
+        if (object.isMesh) {
+            console.log(object.name);
+        }
+    });
+
+    return (
+        <group {...props} dispose={null}>
+            <mesh
+                receiveShadow={true}
+                scale={nodes.Cube031.scale}
+                position={nodes.Cube031.position}
+                geometry={nodes.Cube031.geometry}
+            >
+                <meshStandardMaterial color={'#F3D393'} />
+            </mesh>
+            <mesh
+                receiveShadow={true}
+                scale={nodes.Cube032.scale}
+                position={nodes.Cube032.position}
+                geometry={nodes.Cube032.geometry}
+            >
+                <meshStandardMaterial color={'#F3D393'} />
+            </mesh>
+        </group>
     )
 }
 
 function Tree(props) {
-    const { scene } = useGLTF("./Models/Tree.glb");
-    // scene.traverse((object) => {
-    //     if (object.isMesh) {
-    //         console.log(object.name);
-    //     }
-    // });
+    const { scene, nodes } = useGLTF("./Models/Tree.glb");
 
     return (
-        <>
-            <primitive object={scene} scale={1} position={props.position}/>
-        </>
+        <group {...props} dispose={null}>
+            <mesh
+                receiveShadow={true}
+                scale={nodes.Cylinder001.scale}
+                geometry={nodes.Cylinder001.geometry}
+            >
+                <meshStandardMaterial color={'#F3D393'} />
+            </mesh>
+        </group>
     )
 }
 
@@ -42,8 +119,10 @@ export default function TitleHouse() {
                 <PerspectiveCamera makeDefault />
                 <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
                 <Stage>
-                    <House />
-                    <Tree position={[2,0.22,2]}/>
+                    <House position={[0, 0, 0]} />
+                    <Tree position={[0, 1.05, 0]} />
+                    <Rock position={[5, 0, 0]} />
+                    <Fence1 />
                 </Stage>
             </Suspense>
         </Canvas>
