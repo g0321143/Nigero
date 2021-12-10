18919230570d2c0d4e7aa725c170ff6a3f91d531
ToyotaAkari <94047601+ToyotaAkari@users.noreply.github.com>
2021-12-10 17:25:02 +0900

    コインの位置に合わせました

--- src/Utils/Text.js
+++ src/Utils/Text.js
@@ -2,21 +2,50 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+const Block = styled.div`
+    position: absolute;
+    display:flex;
+    
+    width: 100%;
+    height: 100%;
+
+    font-size: 30px;
+    
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: flex-end;
+`;
+
+
 const ScoreText = styled.div`
-display:flex;
-font-size: 3.5rem;
-color: #B9B179;
-position: absolute;
-top: ${props => props.top};
-left: ${props => props.left};
+  display:flex;
+  font-size: 3vw;
+  color: #B9B179;
+
+  width: 10%;
+  height: 10%;
+  margin: 2%;
+
+
+  font-weight: bold;
+  justify-content: flex-end;
+  align-items: center;
+  text-shadow: 1px 1px 2px silver;
+  
+  user-select: none;
+  user-drag: none;
+  
+  z-index: 999;
 `;
 
 // 未完成です
 function Score({text}) {
     return (
+        <Block>
         <ScoreText>
             {text}
         </ScoreText>
+        </Block>
     );
 }
 
@@ -29,9 +58,7 @@ Score.propTypes = {
 
 // props ごとのデフォルトの値
 Score.defaultProps = {
-    text: 'default',
-    top: '0',
-    left: '0'
+    text: 'default'
 };
 
 export default Score;
