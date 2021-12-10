8d7242e05f878704b6b32c1bf956649985f856c1
ToyotaAkari <94047601+ToyotaAkari@users.noreply.github.com>
2021-12-10 16:11:25 +0900

    ボタンが機能しない

--- src/Scenes/Game.js
+++ src/Scenes/Game.js
@@ -1,142 +1,85 @@
-import React, { useRef, useState, useEffect  } from 'react'
+import React, { Suspense, useRef, useState, useEffect  } from 'react'
 import {useFrame}  from '@react-three/fiber'
 
 import Store from '../Utils/Store';
-import { Game_Canvas, Block_Right_End, Block_Column_End, Block_Column_Top, Block_Left_End, Block_Left_Top } from '../Utils/GlobalStyles';
+import Text from '../Utils/Text';
+import { Game_Canvas, Block_Right_End, Block_Column_End } from '../Utils/GlobalStyles';
 import Button from '../Utils/Button';
 import Score from '../Utils/Score';
 
-import backButton from '../Assets/Images/BUTTONS_EARTHQUAKE_GAME_3-05.png';
+import backButton from '../Assets/Images/GO_BACKWARD.png';
 
-import Coin from '../Utils/Money'
+import Coin from '../Utils/Coin'
 import GameStage from '../Elements/GameStage';
 
 
+import ReactDOM from 'react-dom';
+import Countdown from 'react-countdown';
+
 function Clear(){
+    return( <group>
+        <Coin/>
+        <Block_Column_End>
+        <Score
+                width={'200px'}
+                height={'200px'}
+                star1={true}
+                star2={true}
+                star3={true}
+            />
+        </Block_Column_End>
+    </group>);
+}
+function Model(){
     return(
         <group>
-            <Coin/>
-            <Block_Column_End>
-            <Score
-                    width={'200px'}
-                    height={'200px'}
-                    star1={true}
-                    star2={true}
-                    star3={true}
-                />
-            </Block_Column_End>
+            <Block_Right_End>
+            <Button
+                handler={() => Store.setScene('select')}
+                src={backButton}
+                width={'6%'}
+                height={'10%'}
+                margin={'1%'}
+            />
+            </Block_Right_End>
+            <GameStage /> 
         </group>
-    );
-}
-/*******    ココ↓の引数がNaNになります．．．    *******/
-function Playing({time}){
-    
-    return(
-        <Block_Column_Top>
-                {time/1000}
-        </Block_Column_Top>
-    );
-}
-
-function timecount({time}){
-    time -= 1
-    const timeoutId = setTimeout(timecount, 1000);
-    if(time < 0){
-      clearTimeout(timeoutId);  //timeoutIdをclearTimeoutで指定している
-    }
-
-}
-
-//★ 関数 getRemainTime の定義
-
-function GetRemainTime({time}){
-
-    //★ 今現在の Date オブジェクトを作成
-    var nowDate = new Date()
-    //★ 残りミリ秒数を取得
-    var ms = 5000 - (nowDate.getTime()-time);
-
-    const countUp = () => {
-        //★ 今現在の Date オブジェクトを作成
-        var nowDate = new Date()
-        //★ 残りミリ秒数を取得
-        var ms = 5000 - (nowDate.getTime()-time);
-        const timeoutId = setTimeout(countUp, 1000);
-        if(ms < 0){
-            clearTimeout(timeoutId);    //timeoutIdをclearTimeoutで指定している
-        }
-    }
-    countUp();
-   
-  
-    //★ 残り秒数がなくなれば
-    if(ms <= 0){
-        return(
-            <group>
-                <Coin/>
-                <Block_Column_End>
-                <Score
-                        width={'200px'}
-                        height={'200px'}
-                        star1={true}
-                        star2={true}
-                        star3={true}
-                    />
-                </Block_Column_End>
-            </group>
         );
-    }
-
-    return(
-        <Block_Column_Top>
-                {ms}
-        </Block_Column_Top>
-        
-    );
-
 }
 
-function animate({timing, draw, duration}) {
-
-    let start = performance.now();
-  
-    requestAnimationFrame(function animate(time) {
-      // timeFraction は 0 から 1 になります
-      let timeFraction = (time - start) / duration;
-      if (timeFraction > 1) timeFraction = 1;
-  
-      // 現在のアニメーションの状態を計算します
-      let progress = timing(timeFraction)
-  
-      draw(progress); // 描画します
-  
-      if (timeFraction < 1) {
-        requestAnimationFrame(animate);
-      }
-  
-    });
-}
+// Renderer callback with condition
+const renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+        // Render a completed state
+        return (
+                <Game_Canvas>
+                    <Clear />
+                    <Model />
+                </Game_Canvas>
+        );
+    } else {
+        // Render a countdown
+        const time = minutes*60 + seconds;
+        return (
+                <Game_Canvas>
+                    <Text  text={String(time)}/>
+                    <Model />
+                </Game_Canvas>
+        );
+    }};
 
 
 export default function Game() {
-    const [flag, setflag] = useState(true);
-    var nowDate = new Date()
-    const [count, settime] = useState(nowDate.getTime());
-
+    
     return (
-        <Game_Canvas>
-            <GetRemainTime time = {count}/>
-            <Block_Right_End>
-                <Button
-                    handler={() => Store.setScene('select')}
-                    src={backButton}
-                    width={'6%'}
-                    height={'10%'}
-                    margin={'1%'}
-                />
-            </Block_Right_End>
-            <GameStage />
-            
+        <Game_Canvas> 
+            {ReactDOM.render(
+            <Countdown
+                date={Date.now() + 5000}
+                renderer={renderer}
+            />,
+            document.getElementById('root')
+            )}    
         </Game_Canvas>
     );
 }
