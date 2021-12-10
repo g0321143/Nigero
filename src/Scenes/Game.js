70f6fefa834b9176a20d6b7a29955c16362f03a9
ToyotaAkari <94047601+ToyotaAkari@users.noreply.github.com>
2021-12-10 13:05:33 +0900

    リアルタイムでタイマー表示ができない

--- src/Scenes/Game.js
+++ src/Scenes/Game.js
@@ -30,8 +30,7 @@ function Clear(){
 }
 /*******    ココ↓の引数がNaNになります．．．    *******/
 function Playing({time}){
-    //const [time, settime] = useState(5000);
-
+    
     return(
         <Block_Column_Top>
                 {time/1000}
@@ -39,17 +38,94 @@ function Playing({time}){
     );
 }
 
-function TimeCount(time){
-    time -= 1;
-    return(time);
+function timecount({time}){
+    time -= 1
+    const timeoutId = setTimeout(timecount, 1000);
+    if(time < 0){
+      clearTimeout(timeoutId);  //timeoutIdをclearTimeoutで指定している
+    }
+
 }
+
+//★ 関数 getRemainTime の定義
+
+function GetRemainTime({time}){
+
+    //★ 今現在の Date オブジェクトを作成
+    var nowDate = new Date()
+    //★ 残りミリ秒数を取得
+    var ms = 5000 - (nowDate.getTime()-time);
+
+    const countUp = () => {
+        //★ 今現在の Date オブジェクトを作成
+        var nowDate = new Date()
+        //★ 残りミリ秒数を取得
+        var ms = 5000 - (nowDate.getTime()-time);
+        const timeoutId = setTimeout(countUp, 1000);
+        if(ms < 0){
+            clearTimeout(timeoutId);    //timeoutIdをclearTimeoutで指定している
+        }
+    }
+    countUp();
+   
+  
+    //★ 残り秒数がなくなれば
+    if(ms <= 0){
+        return(
+            <group>
+                <Coin/>
+                <Block_Column_End>
+                <Score
+                        width={'200px'}
+                        height={'200px'}
+                        star1={true}
+                        star2={true}
+                        star3={true}
+                    />
+                </Block_Column_End>
+            </group>
+        );
+    }
+
+    return(
+        <Block_Column_Top>
+                {ms}
+        </Block_Column_Top>
+        
+    );
+
+}
+
+function animate({timing, draw, duration}) {
+
+    let start = performance.now();
+  
+    requestAnimationFrame(function animate(time) {
+      // timeFraction は 0 から 1 になります
+      let timeFraction = (time - start) / duration;
+      if (timeFraction > 1) timeFraction = 1;
+  
+      // 現在のアニメーションの状態を計算します
+      let progress = timing(timeFraction)
+  
+      draw(progress); // 描画します
+  
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+  
+    });
+}
+
 
 export default function Game() {
     const [flag, setflag] = useState(true);
-    const [count, settime] = useState(5000);
+    var nowDate = new Date()
+    const [count, settime] = useState(nowDate.getTime());
+
     return (
         <Game_Canvas>
-            {flag ? <Playing time={count}/> : <Clear/>}
+            <GetRemainTime time = {count}/>
             <Block_Right_End>
                 <Button
                     handler={() => Store.setScene('select')}
