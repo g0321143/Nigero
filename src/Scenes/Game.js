8ab29247178671bd25bf5162bae5cf7ab3d81843
ToyotaAkari <toyota@scv.cis.iwate-u.ac.jp>
2021-12-14 17:47:21 +0900

    クリア時間追加

--- src/Scenes/Game.js
+++ src/Scenes/Game.js
@@ -32,7 +32,7 @@ function Clear() {
     return (
         <>
             <Money />
-            <TimeText fontsize={"5vw"} top={"5%"} left={"35%"}>{":COMPLETE:"}</TimeText>
+            <AnyText fontsize={"5vw"} top={"5%"} left={"35%"}>{":COMPLETE:"}</AnyText>
             <Block_Column_End>
                 <StarScore
                     width={'200px'}
@@ -41,10 +41,10 @@ function Clear() {
                     star2={true}
                     star3={true}
                 />
-            </Block_Column_End>
-            <ChooserButton onClick={() => Store.setScene('select')} src={nextButton} top={"25%"} left={"80%"} />
-            <ChooserButton onClick={() => Store.resetGame()} src={retyrButton} top={"35%"} left={"80%"} />
-            <ChooserButton onClick={() => Store.setScene('select')} src={tipsButton} top={"45%"} left={"80%"} />
+            </Block_Column_End>          
+            <Setting onClick={() => Store.setScene('select')} src={nextButton} top={"25%"} left={"80%"} width={'15%'} height={'15%'} opacity={'0.9'}/>
+            <Setting onClick={() => Store.setScene('game')} src={retyrButton} top={"35%"} left={"80%"} width={'15%'} height={'15%'} opacity={'0.9'}/>
+            <Setting onClick={() => Store.setScene('select')} src={tipsButton} top={"45%"} left={"80%"} width={'15%'} height={'15%'} opacity={'0.9'}/>
         </>
     );
 }
@@ -52,7 +52,7 @@ function Clear() {
 function Playing(){
     return(
         <group>
-            <ItemBox src={item}/>
+            <Setting src={item} top={"20%"} left={"80%"} width={'20%'} height={'50%'} opacity={'0.9'}/>
         </group>
     );
 }
@@ -63,10 +63,10 @@ function Model() {
 
     return (
         <>
-            <MissonBox src={mission} />
-            <TimeText fontsize={"0.8vw"} top={"35.3%"} left={"6%"}>{"Securing of personal security"}</TimeText>
-            <TimeText fontsize={"1vw"} top={"39.3%"} left={"6%"}>{"Check of the fall"}</TimeText>
-            <TimeText fontsize={"1vw"} top={"43.5%"} left={"6%"}>{"ほげ～"}</TimeText>
+            <Setting src={mission} top={"20%"} left={"0%"} width={'20%'} height={'50%'} opacity={'0.9'}/>
+            <AnyText fontsize={"0.8vw"} top={"35.3%"} left={"6%"}>{"Securing of personal security"}</AnyText>
+            <AnyText fontsize={"1vw"} top={"39.3%"} left={"6%"}>{"Check of the fall"}</AnyText>
+            <AnyText fontsize={"1vw"} top={"43.5%"} left={"6%"}>{"ほげ～"}</AnyText>
             <Check />
             <Block_Right_End>
                 <Button
@@ -88,27 +88,41 @@ function Check() {
     const flag3 = true;
     return (
         <>
-            {flag1 ? <CheckBox src={checkmark} top={"32%"} left={"14.5%"} /> : null}
-            {flag2 ? <CheckBox src={checkmark} top={"36.5%"} left={"14.5%"} /> : null}
-            {flag3 ? <CheckBox src={checkmark} top={"41%"} left={"14.5%"} /> : null}
+            {flag1 ? <Setting src={checkmark} top={"32%"} left={"14.5%"} width={'3%'} height={'3%'} opacity={'1'}/> : null}
+            {flag2 ? <Setting src={checkmark} top={"36.5%"} left={"14.5%"} width={'3%'} height={'3%'} opacity={'1'}/> : null}
+            {flag3 ? <Setting src={checkmark} top={"41%"} left={"14.5%"} width={'3%'} height={'3%'} opacity={'1'}/> : null}
+            
         </>
     );
+}
+
+function ClearTime({time, limit}){
+    const [time_s, settime_s] = useState(parseInt((limit - time) % 60));
+    const [usertime_s, setusertime_s] = useState(( '00' + time_s ).slice( -2 ));
+    const [time_m, settime_m] = useState(parseInt((limit - time) / 60));
+    const [usertime_m, setusertime_m] = useState(( '00' + time_m ).slice( -2 ));
+
+    return(<AnyText fontsize={"5vw"} top={"70%"} left={"44%"}>{usertime_m + ":" + usertime_s}</AnyText>);
 }
 
 
 
 // Renderer callback with condition
-const renderer = ({ hours, minutes, seconds, completed }) => {
-    if (completed) {
+const renderer = ({ hours, minutes, seconds, completed}) => {
+    const time = minutes * 60 + seconds;
+    const limit = 100000;
+
+   
+    if (completed || time <= 99) {
         // Render a completed state
         return (
             <>
+                <ClearTime time={time} limit={limit/1000}/>
                 <Clear />
             </>
         );
     } else {
         // Render a countdown
-        const time = minutes * 60 + seconds;
         return (
             <>
                 <Text text={String(time)} />
@@ -120,11 +134,13 @@ const renderer = ({ hours, minutes, seconds, completed }) => {
 
 
 export default class Game extends React.Component {
-
+    /*limit: 制限時間
+    ココを変更するときはconst renderer 内のlimitも変更*/
     constructor(props) {
         super(props);
         this.state = {
-            key: false
+            key: false,
+            limit: 100000
         };
     }
 
@@ -151,10 +167,11 @@ export default class Game extends React.Component {
     render() {
         return (
             <Game_Canvas >
+                
                 <Countdown
-                    date={Date.now() + 5000}
+                    date={Date.now() + this.state.limit}
                     renderer={renderer}
-                    key={this.state.key}
+                    key={this.state.key}        
                 />
                 <Model />
             </Game_Canvas>
@@ -162,11 +179,11 @@ export default class Game extends React.Component {
     }
 }
 
-const ChooserButton = styled.div`
+const Setting = styled.div`
   display:flex;
   position: absolute;
-  width: 15%;
-  height: 15%;
+  width: ${props => props.width};
+  height: ${props => props.height};
   
   margin: ${props => props.margin};
   top: ${(props) => props.top};
@@ -176,7 +193,7 @@ const ChooserButton = styled.div`
   background-size: contain;
   background-repeat: no-repeat;
   background-position: center center;
-  opacity: 0.9;
+  opacity: ${(props) => props.opacity};
   z-index: 999;
 
   :hover {
@@ -185,7 +202,7 @@ const ChooserButton = styled.div`
   }
 `;
 
-const TimeText = styled.div`
+const AnyText = styled.div`
     display: flex;
     position: absolute;
     font-size: ${(props) => props.fontsize};
@@ -197,97 +214,4 @@ const TimeText = styled.div`
     left: ${(props) => props.left};
 
     z-index: 999;
-`;
-
-const CheckBox = styled.div`
-    position: absolute;
-    margin: 1vw;
-
-    border-radius: 5%;
-    width: 3%;
-    height: 3%;
-    
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    top: ${(props) => props.top};
-    left: ${(props) => props.left};
-
-    background-image: url(${props => props.src});
-
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center center;
-    z-index: 999;
-
-    :hover {
-        cursor: pointer;
-        opacity: 1;
-  }
-    
-`;
-
-const MissonBox = styled.div`
-    position: absolute;
-    margin: 1vw;
-
-    border-radius: 5%;
-    width: 20%;
-    height: 50%;
-    
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    top: 20%;
-    left: 0%;
-
-    background-image: url(${props => props.src});
-
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center center;
-    opacity: 0.9;
-    z-index: 999;
-
-    :hover {
-        cursor: pointer;
-        opacity: 1;
-  }
-    
-`;
-
-
-const ItemBox = styled.div`
-    position: absolute;
-    margin: 1vw;
-
-    border-radius: 5%;
-    width: 20%;
-    height: 50%;
-    
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    top: 20%;
-    left: 78%;
-
-    background-image: url(${props => props.src});
-
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center center;
-    opacity: 0.9;
-    z-index: 999;
-
-    :hover {
-        cursor: pointer;
-        opacity: 1;
-  }
-    
 `;
