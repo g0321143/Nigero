d25c1a47d0601675eee16c5d9d7cf92c4be5505a
g0321143 <g0321143@iwate-u.ac.jp>
2022-01-22 18:17:06 +0900

    セレクト画面に渡されてた引数消されていたので，戻しました

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ class App extends React.Component {
 
         switch (scene) {
             case 'title': return <Title />;
-            case 'select': return <Select />;
+            case 'select': return <Select currentBuilding={building}/>;
             case 'option': return <Option />;
             case 'itemShop': return <ItemShop />;
             case 'game': return <Game building={building} stage={stage} />;
